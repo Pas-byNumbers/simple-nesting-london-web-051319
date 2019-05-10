03b82b3e9ac652ba95fe859f86ba80020997f536
Pascal Rassaby <pasbynumbers@gmail.com>
2019-05-10 17:31:14 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -130,7 +130,7 @@ def adding_to_dennis
         }
      }
      
-    return programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+    return programmer_hash[:dennis_ritchie][:languages].push("Assembly")
      
 
 end
